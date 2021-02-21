4c2524d5c53d97c205f29270da31117ad38e8418
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-02-21 00:47:23 -0700

    Python: added basic test

---
+++ py_test.py
@@ -0,0 +1,15 @@
+from Python.pykv import *
+
+kv = KVFile();
+kv.add(3.14159, "pi")
+kv.add(12.05, "Vin", "Input Voltage (V)")
+kv.add([12.05, 12.03, 11.97], "Vin_t", "Vin logged (V)")
+if not kv.add(None, "This_fails"):
+	print(kv.err())
+if not kv.add([[[1]]], "This_also_fails"):
+	print(kv.err())
+if not kv.add([1], "pi"):
+	print(kv.err())
+print(kv("pi"))
+print(kv("Vin"))
+print(kv("Vin_t"))
