1053b5e2d6dde0b9c0597727b934203f93c5bb14
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-03-22 13:20:20 -0600

    MATLAB: Added getString() function

---
+++ MATLAB/getString.m
@@ -0,0 +1,32 @@
+function out=getString(str)
+
+	str = char(str);
+
+	escapes = find(str=='\');
+	quotes = find(str=='"');
+	se_quotes = []; % list of start or end (string) quotes
+	for i=1:length(quotes)
+		
+		%If index before index of quote *is not* found in escapes, this
+		%quote is *not* escaped and thus is added to the list of
+		%legitamate quotes
+		if isempty(find(escapes == (quotes(i)-1))) 
+			if isempty(se_quotes)
+				se_quotes = quotes(i);
+			else
+				se_quotes(end+1) = quotes(i);
+			end
+		end
+	end
+
+	%Return empty if no quote present
+	if isempty(se_quotes) || length(se_quotes) < 2
+		out=[];
+		return;
+	end
+	
+	startIdx = se_quotes(1);
+	endIdx = se_quotes(2);
+	out=StringIdx(str(startIdx:endIdx), endIdx);
+	
+end
