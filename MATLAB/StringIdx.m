76584c26cae25e997a05c1c8abd4e91b3572a395
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-04-28 20:50:41 -0600

    StringIdx: added doc strings and 'sidx' variable in case two indeces are required

--- MATLAB/StringIdx.m
+++ MATLAB/StringIdx.m
@@ -1,12 +1,20 @@
 classdef StringIdx
+% StringIdx String object for representing substrings
+%	String object with start and end indecies referring to its
+%	location in a larger string.
+
 	properties
-		str
-		idx
+		str % String value
+		idx % Index where string value ended in some larger string
+		sidx
 	end
 	methods
 		function obj=StringIdx(val, start_idx)
+			% Constructor
+			
 			obj.str = string(val);
 			obj.idx = start_idx;
+			obj.sidx = -1;
 		end
 	end
 end
