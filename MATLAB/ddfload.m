e0a6c73f23ebecb5559ef7a145a54ca6ff2f9621
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-05-23 21:39:14 -0600

    MATLAB ddfload: Changed -eng to -elec, added doc string, added -which flag, added filename option

--- MATLAB/ddfload.m
+++ MATLAB/ddfload.m
@@ -1,9 +1,28 @@
 function ddfload(varargin)
-
+%DDFLOAD Loads data from a DDF file saved in the data directory.
+%
+%	DDFLOAD(FILENAME) Looks for a file with the name FILENAME in the data
+%	directory.
+%
+%	DDFLOAD(FLAGS...) Loads the data specified by the given flag(s) into
+%	the workspace.
+%
+%	 Flag		Meaning
+%	----------  --------------------------------
+%	    -where	Prints the location of the data directory the configuration
+%			    file, and this function.
+%	      -sci	Load "scientific.ddf" which contains a selection of commonly
+%				used constants in the pysical sciences.
+%	     -elec	Load "elec_eng.ddf" which contains a selection of commonly used
+%				constants in electrical engineering.
+%	      -fav	Load "favorites.ddf". This file is user customizable to include
+%				the user's most used variables.
+%
+%	See also DDFIO.
 	
-	local_dir = mfilename('fullpath'); % Get full path to this file
-	trim_idx = find(local_dir=='/', 1, 'last'); % Find index for trimming to directory
-	local_dir = local_dir(1:trim_idx-1);
+	file_path = mfilename('fullpath'); % Get full path to this file
+	trim_idx = find(file_path=='/', 1, 'last'); % Find index for trimming to directory
+	local_dir = file_path(1:trim_idx-1);
 	
 	% ====================== NOTE TO USER =================================
 	% Settings file for ddfload must be saved here, or this string must be
@@ -13,7 +32,7 @@ function ddfload(varargin)
 	% the call to fullfile and instead specify the absolute path as a
 	% string.
 	%
-	settings_file = fullfile(local_dir, "../Data/ddfload.conf");
+	settings_file = fullfile(local_dir, "../ddfload_Data/ddfload.conf");
 
 	% Read config file
 	confs = loadConfig(settings_file, 'ShowMessages', false);
@@ -31,6 +50,8 @@ function ddfload(varargin)
 	%Load data
 	for shortcut=varargin
 
+		skip_save = false;
+		csc = char(shortcut);
 		shortcut = string(shortcut);
 		datafile_name = "favorites.ddf";
 		workspace_abbrev = "fav";
@@ -50,32 +71,72 @@ function ddfload(varargin)
 			datafile_name = "favorites.ddf";
 			workspace_abbrev = "fav";
 			
-		elseif strcmp(shortcut, "-eng")
+		elseif strcmp(shortcut, "-elec")
 			
-			datafile_name = "engineering.ddf";
-			workspace_abbrev = "eng";
+			datafile_name = "elec_eng.ddf";
+			workspace_abbrev = "elec";
+			
+		elseif strcmp(shortcut, "-where")
+			
+			%Prevent saving variables
+			skip_save = true;
+			
+			%Display paths
+			displ("    Configuration File Path: '", settings_file, "'");
+			displ("    Data Directory: '", confs('datapath') , "'");
+			displ("    Path to this function: '", file_path, ".m'");
+		
+		elseif csc(1) ~= '-' % Interpret as DDF filename
+			
+			% Parse file
+			[filepath, name, ext] = fileparts(shortcut);
+			
+			% If no extension provided, add .ddf
+			% TODO: Allow user to override and force extension free file to
+			% be readable.
+			if strcmp(ext, "")
+				shortcut = strcat(shortcut, ".ddf");
+				ext = ".ddf";
+			end
+			
+			% Check that file exists
+			if ~isfile(fullfile(confs('datapath'), shortcut))
+				displ("ERROR: Failed to find file '", shortcut, "'.");
+				continue;
+			end
+			
+			% Specify how to read the file
+			datafile_name = strcat(name, ext);
+			workspace_abbrev = name;
 			
 		else 
+			
+			%Prevent saving variables
+			skip_save = true;
 			
 			displ("ERROR: Flag '", shortcut, "' not recognized.");
 			return;
 			
 		end
 		
-		%Get path
-		filepath = fullfile(confs('datapath'), datafile_name);
+		if ~skip_save
+			
+			%Get path to data directory
+			filepath = fullfile(confs('datapath'), datafile_name);
 
-		%Read data file
-		ddf = DDFIO(filepath);
-		if ddf.numVar() == 0
-			displ("ERROR: Failed to load ", datafile_name, " (", filepath , ").");
+			%Read data file
+			ddf = DDFIO(filepath);
+			if ddf.numVar() == 0
+				displ("ERROR: Failed to load ", datafile_name, " (", filepath , ").");
+				assignin('base', strcat("ddf_", string(workspace_abbrev)), ddf);
+				return
+			end
+
+			% Assign in workspace
+			ddf.assignAll();
 			assignin('base', strcat("ddf_", string(workspace_abbrev)), ddf);
-			return
+			
 		end
-
-		% Assign in workspace
-		ddf.assignAll();
-		assignin('base', strcat("ddf_", string(workspace_abbrev)), ddf);
 	end
 	
 end
